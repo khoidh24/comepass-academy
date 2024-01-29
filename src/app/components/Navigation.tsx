084d85d27f1526b77bf0085bf11f02c3ac03539c
khoidh24 <65653624+khoidh24@users.noreply.github.com>
2024-01-29 12:47:12 +0700

    Update Navigation.tsx

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -87,7 +87,7 @@ const App: React.FC = () => {
   }
 
   return (
-    <div className='nav--container sticky z-[1000]'>
+    <div className='nav--container sticky z-50'>
       <nav className={`nav--pc ${lexendDeca.className}`}>
         {/*PENDING Mobile & Tablet Screen*/}
         <div className='nav--mobile__button'>
